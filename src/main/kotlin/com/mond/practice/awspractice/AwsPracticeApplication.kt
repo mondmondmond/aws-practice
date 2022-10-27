bd62f77c15a27782668bac3c60e3f4cd74cd80b6
YOUNGGEON LEE <revive29@gmail.com>
2022-10-27 20:35:35 +0900

    chore: init project

---
+++ src/main/kotlin/com/mond/practice/awspractice/AwsPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.mond.practice.awspractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AwsPracticeApplication
+
+fun main(args: Array<String>) {
+	runApplication<AwsPracticeApplication>(*args)
+}
